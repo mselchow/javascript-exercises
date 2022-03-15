ada1e550877fb49543ad635720e373263daf83f1
Michael Selchow <mselchow@pm.me>
2022-03-15 19:28:32 -0400

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,28 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+    let startNum, endNum;
 
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
+
+    if (typeof (start) != "number" || typeof (end) != "number") {
+        return "ERROR";
+    }
+
+    if (start > end) {
+        startNum = end;
+        endNum = start;
+    } else {
+        startNum = start;
+        endNum = end;
+    }
+
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
