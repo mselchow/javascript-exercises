5bac244b942849cd2b5060011b02897d189d62aa
Michael Selchow <mselchow@pm.me>
2022-03-15 19:19:56 -0400

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    let tmp = arguments[0];
+    let index;
 
+    for (let i = 1; i < arguments.length; i++) {
+        index = tmp.indexOf(arguments[i]);
+        if (index > -1) {
+          tmp.splice(index, 1);
+        }
+    }
+
+    return tmp;
 };
 
 // Do not edit below this line
