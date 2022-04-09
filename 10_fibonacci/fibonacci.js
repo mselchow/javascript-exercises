810090b88c6c55a81782d28cae0b30b595f600c2
Michael Selchow <mselchow@pm.me>
2022-04-09 10:10:37 -0400

    Complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function (term) {
+  if (term >= 0) {
+    const phi = (1 + Math.sqrt(5)) / 2;
+    return Math.round(
+      (Math.pow(phi, term) - Math.pow(1 - phi, term)) / Math.sqrt(5)
+    );
+  } else {
+    return "OOPS";
+  }
 };
 
 // Do not edit below this line
