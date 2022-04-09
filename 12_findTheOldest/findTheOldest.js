fe9821d365a81ca6a0130039a4ddd466b761c220
Michael Selchow <mselchow@pm.me>
2022-04-09 10:35:02 -0400

    Complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+  people.sort((a, b) => {
+    const aDeath = a.yearOfDeath ? a.yearOfDeath : new Date().getFullYear();
+    const bDeath = b.yearOfDeath ? b.yearOfDeath : new Date().getFullYear();
+    return aDeath - a.yearOfBirth > bDeath - b.yearOfBirth ? -1 : 1;
+  });
+  return people[0];
 };
 
 // Do not edit below this line
