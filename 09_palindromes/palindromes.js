a5b3ce25cf938513d66761b6250ccca4da182bb6
Michael Selchow <mselchow@pm.me>
2022-04-09 10:02:37 -0400

    Complete palindrome function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const pal = word.toLowerCase().replaceAll(/[\s\W]/g, "");
+  let reverse = pal;
+  reverse = reverse.split("").reverse().join("");
+  return pal === reverse;
 };
 
 // Do not edit below this line
